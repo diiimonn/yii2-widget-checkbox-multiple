db87bb7052ed15757eb6a952ba05a510a5da9f87
dima <diiimonn@gmail.com>
2015-02-05 10:48:29 +0200

    fix composer & assets

--- CheckboxMultipleAsset.php
+++ CheckboxMultipleAsset.php
@@ -12,7 +12,6 @@ class CheckboxMultipleAsset extends AssetBundle
     public $sourcePath = '@vendor/diiimonn/yii2-widget-checkbox-multiple/assets';
 
     public $depends = [
-        'diiimonn\assets\SpinnerCanvasAsset',
         'diiimonn\assets\SlimScrollAsset'
     ];
 
